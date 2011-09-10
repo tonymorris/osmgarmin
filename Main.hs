26cab6d3925e69f2e257f0e6c5d3b2828958d2ac
Tony Morris <tmorris@tmorris.net>
2011-09-10 15:34:58 +1000

    code formatting

--- Main.hs
+++ Main.hs
@@ -8,16 +8,22 @@ import Data.Time.Clock
 import Data.Time.Calendar
 import Control.Applicative
 
-maps :: [Map]
-maps = [Map {
-  mtype = OpenStreetMap (mapname' "82345912"),
-  compression = Bzip,
-  source = URL "http://download.geofabrik.de/osm/australia-oceania.osm.bz2"
-}, Map {
-  mtype = Garmin,
-  compression = None,
-  source = URL "http://projects.tmorris.net/public/maps/scratch/Mount%20Barney/Brek/Mt_Barney_National_Park_Contours_Streams_etc.img"
-}]
+maps ::
+  [Map]
+maps =
+  [
+    Map {
+      mtype = OpenStreetMap (mapname' "82345912"),
+      compression = Bzip,
+      source = URL "http://download.geofabrik.de/osm/australia-oceania.osm.bz2"
+    }
+  , 
+    Map {
+      mtype = Garmin,
+      compression = None,
+      source = URL "http://projects.tmorris.net/public/maps/scratch/Mount%20Barney/Brek/Mt_Barney_National_Park_Contours_Streams_etc.img"
+    }
+  ]
 
 main :: IO ()
 main = resolveMaps maps (\p e -> if e == ExitSuccess
