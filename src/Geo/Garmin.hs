8d2d1704221bd0dd2de13649c54114d25dc71ca2
Tony Morris <tmorris@tmorris.net>
2015-06-21 23:12:34 +1000

    sys-process

--- src/Geo/Garmin.hs
+++ src/Geo/Garmin.hs
@@ -1,285 +1,204 @@
-module Geo.Garmin where
+module Geo.Garmin(
+  time
+, downloadDirectory
+, buildDirectory
+, distDirectory
+, australiaOceaniaPbf
+, mkgmap
+, splitter
+, getAustraliaOceania
+, splitAustraliaOceania
+, gmapsuppAustraliaOceania
+, linkAustraliaOceania
+, getMountBarney
+, gmapsuppMountBarneyAustraliaOceania
+, linkAustraliaOceaniaMountBarney
+, commands
+, linkLatest
+) where
 
-import Data.Time
-import Data.Char
-import Data.List
-import System.Process
-import System.Exit
-import System.Directory
-import System.FilePath
-import Control.Monad
-import Control.Exception
+import Data.Time(UTCTime(utctDay, utctDayTime), TimeOfDay(TimeOfDay), toGregorian, timeToTimeOfDay)
+import Sys.Exit(CreateProcess, procIn)
+import System.FilePath((</>))
 
-newtype MapName = MapName {
-  mid :: String
-} deriving (Eq, Ord, Show)
-
-mapname' ::
-  String
-  -> MapName
-mapname' s =
-  if length s == 8 && all isDigit s
-    then MapName s
-    else error ("Invalid mapname: " ++ s)
-
-incrementMapName ::
-  MapName
-  -> MapName
-incrementMapName =
-  mapname' . show . flip (((+) :: Integer -> Integer -> Integer) . read . mid) 1
-
-data MapType =
-  OpenStreetMap {
-    osmmapname :: MapName
-  } | Pbf {
-    pbfmapname :: MapName
-  } | Garmin -- todo Polish
-  deriving (Eq, Show)
-
-foldMapType ::
-  (MapName -> a)
-  -> (MapName -> a)
-  -> a
-  -> MapType
-  -> a
-foldMapType o _ _ (OpenStreetMap i) =
-  o i
-foldMapType _ p _ (Pbf i) =
-  p i
-foldMapType _ _ g Garmin =
-  g
-
-extension ::
-  Compression
-  -> MapType
+time ::
+  UTCTime
   -> String
-extension c =
-  let com None = id
-      com Bzip = (++ ".bz2")
-      com Gzip = (++ ".gz")
-      com Zip  = (++ ".zip")
-  in com c . foldMapType (const ".osm") (const ".pbf") ".img"
+time t =
+  let show2 = let s2 [x] = ['0', x]
+                  s2 x = x
+              in s2 . show
+      (y, m, d) = toGregorian (utctDay t)
+      TimeOfDay h n s = timeToTimeOfDay (utctDayTime t)
+  in concat [show y, show2 m, show2 d, "-", show2 h, show2 n, show2 (floor s)]
 
-extension' ::
-  Map
-  -> String
-extension' =
-  extension <$> compression <*> mtype
-
-data Compression =
-  None
-  | Bzip
-  | Gzip
-  | Zip
-  deriving (Eq, Show)
-
-data Source =
-  File {
-    file :: FilePath
-  } |
-  URL {
-    url :: String
-  }
-  deriving (Eq, Show)
-
-foldSource ::
-  (FilePath -> a)
-  -> (String -> a)
-  -> Source
-  -> a
-foldSource f _ (File f') =
-  f f'
-foldSource _ u (URL u') =
-  u u'
-
-data Map =
-  Map {
-    mtype :: MapType,
-    compression :: Compression,
-    source :: Source
-  }
-  deriving (Eq, Show)
-
-findM ::
-  Monad m =>
-  (a -> m Bool)
-  -> [a]
-  -> m (Maybe a)
-findM _ [] =  
-  return Nothing
-findM p (x:xs) =
-  p x >>= \z -> if z
-                  then return (Just x)
-                  else findM p xs
-
-loop_ ::
-  Monad m =>
-  (a -> m Bool)
-  -> (a -> m b)
-  -> [a]
-  -> m ()
-loop_ _ _ [] =
-  return ();
-loop_ p a  (x:xs) =
-  p x >>= flip when (a x >> loop_ p a xs)
-
-work ::
-  IO FilePath
-work =
-  do _ <- getTemporaryDirectory
-     let d = "/home/tmorris/Desktop" </> "osmgarmin"
-     mkdir d
-     return d
-
-exists ::
+downloadDirectory ::
   FilePath
-  -> IO Bool
-exists d =
-  (||) <$> doesFileExist d <*> doesDirectoryExist d
+downloadDirectory =
+  "download"
 
-system' ::
-  String
-  -> String
-  -> IO ExitCode
-system' c s =
-  do let r = c ++ ' ' : s
-     d <- getCurrentDirectory
-     appendFile "/home/tmorris/Desktop/osmgarmin.out" (">>> " ++ d ++ "\n")
-     appendFile "/home/tmorris/Desktop/osmgarmin.out" (r ++ "\n")
-     e <- system r
-     return e
+buildDirectory ::
+  FilePath
+buildDirectory =
+  "build"
 
-bzip ::
-  String
-  -> IO ExitCode
-bzip =
-  system' "bzip2"
+distDirectory ::
+  FilePath
+distDirectory =
+  "dist"
 
-wget ::
-  String
-  -> IO ExitCode
-wget =
-  system' "wget -q"
-
-java ::
-  String
-  -> IO ExitCode
-java =
-  system' "java -Xmx1536M"
-
-splitter ::
-  String -> IO ExitCode
-splitter s =
-  java ("-jar /home/tmorris/opt/splitter/splitter.jar " ++ s)
+australiaOceaniaPbf ::
+  FilePath
+australiaOceaniaPbf =
+  "australia-oceania.osm.pbf"
 
 mkgmap ::
-  String
-  -> IO ExitCode
-mkgmap s =
-  java ("-jar /home/tmorris/opt/mkgmap/mkgmap.jar " ++  s)
-
--- | Change to the given directory, then execute the given action, then change back to the original directory.
-chdir ::
-  FilePath -- ^ The directory to change to.
-  -> IO a  -- ^ The action to execute in the given directory.
-  -> IO a  -- ^ The result of executing the given action.
-chdir d a =
-  bracket getCurrentDirectory setCurrentDirectory (\_ -> setCurrentDirectory d >> a)
-
-rm ::
-  FilePath -> IO ()
-rm =
-  (>>=) <$> doesFileExist <*> flip when . removeFile
-
-mkdir ::
   FilePath
-  -> IO ()
-mkdir =
-  createDirectoryIfMissing True
+mkgmap =
+  "opt" </> "mkgmap" </> "mkgmap.jar"
 
-chdirw ::
-  IO a
-  -> IO a
-chdirw =
-  (work >>=) . flip chdir
+splitter ::
+  FilePath
+splitter =
+  "opt" </> "splitter" </> "splitter.jar"
 
-withWorkFile ::
-  String
+getAustraliaOceania ::
+  FilePath
+  -> CreateProcess
+getAustraliaOceania wd =
+  procIn (wd </> downloadDirectory) "wget"
+    [
+      "-q"
+    , "-c"
+    -- http://download.geofabrik.de/australia-oceania-latest.osm.pbf
+    , "http://localhost:8000/map.pbf"
+    , "-O"
+    , australiaOceaniaPbf
+    ]
+    
+splitAustraliaOceania ::
+  FilePath
+  -> CreateProcess
+splitAustraliaOceania wd =
+  procIn (wd </> buildDirectory </> "australia-oceania") "java"
+    [
+      "-Xmx1536M"
+    , "-jar"
+    , splitter
+    , wd </> downloadDirectory </> australiaOceaniaPbf
+    , "--mapid=82345912"
+    ]
+
+gmapsuppAustraliaOceania ::
+  FilePath
+  -> CreateProcess
+gmapsuppAustraliaOceania wd =
+  procIn (wd </> buildDirectory </> "australia-oceania") "java"
+    [
+      "-Xmx1536M"
+    , "-jar"
+    , mkgmap
+    , "--add-pois-to-areas"
+    , "--reduce-point-density-polygon=8"
+    , "--remove-short-arcs"
+    , "--route"
+    , "--transparent"
+    , "--gmapsupp"
+    , "-c"
+    , "template.args"
+    , "--description=\"Australia and Oceania\""
+    , "--country-name=\"Australia and Oceania\""
+    , "--region-name=\"Australia and Oceania\""
+    , "--region-abbr=AU"
+    , "--country-abbr=AU"
+    , "--drive-on=left"
+    , "--check-roundabouts"
+    ]
+
+linkAustraliaOceania ::
+  FilePath
+  -> CreateProcess
+linkAustraliaOceania wd =
+  procIn (wd </> distDirectory </> "australia-oceania") "ln"
+    [
+      "-s"
+    , ".." </> ".." </> buildDirectory </> "australia-oceania" </> "gmapsupp.img"
+    ]
+
+getMountBarney ::
+  FilePath
+  -> CreateProcess    
+getMountBarney wd =
+  procIn (wd </> downloadDirectory) "wget"
+    [
+      "-q"
+    , "-c"
+    , "https://dl.dropboxusercontent.com/u/7810909/img/Mt_Barney_National_Park.img"
+    , "-O"
+    , "mt-barney-national-park.img"
+    ]
+
+gmapsuppMountBarneyAustraliaOceania ::
+  FilePath
+  -> CreateProcess
+gmapsuppMountBarneyAustraliaOceania wd =
+  procIn (wd </> buildDirectory </> "australia-oceania_mt-barney") "java"
+    [
+      "-Xmx1536M"
+     , "-jar"
+     , mkgmap
+     , "--add-pois-to-areas"
+     , "--reduce-point-density-polygon=8"
+     , "--remove-short-arcs"
+     , "--route"
+     , "--transparent"
+     , "--gmapsupp"
+     , "--description=\"Australia and Oceania and Mt Barney Contour\""
+     , "--country-name=\"Australia and Oceania\""
+     , "--country-abbr=AU"
+     , "--region-name=\"Australia and Oceania\""
+     , "--region-abbr=AU"
+     , "--drive-on=left"
+     , "--check-roundabouts"
+     , wd </> buildDirectory </> "australia-oceania" </> "gmapsupp.img"
+     , wd </> downloadDirectory </> "mt-barney-national-park.img"
+     ]
+
+linkAustraliaOceaniaMountBarney ::
+  FilePath
+  -> CreateProcess
+linkAustraliaOceaniaMountBarney wd =
+  procIn (wd </> distDirectory </> "australia-oceania_mt-barney") "ln"
+    [
+      "-s"
+    , ".." </> ".." </> buildDirectory </> "australia-oceania_mt-barney" </> "gmapsupp.img"
+    ]
+
+commands ::
+  FilePath
+  -> [CreateProcess]
+commands =
+  sequence
+    [
+      getAustraliaOceania
+    , splitAustraliaOceania
+    , gmapsuppAustraliaOceania
+    , linkAustraliaOceania
+    , getMountBarney
+    , gmapsuppMountBarneyAustraliaOceania
+    , linkAustraliaOceaniaMountBarney
+    ]
+
+linkLatest ::
+  FilePath
   -> String
-  -> (FilePath -> IO a)
-  -> IO FilePath
-withWorkFile pre post f =
-  do _ <- work
-     t <- getCurrentTime
-     let k = pre ++ ((\c -> if isSpace c then '_' else c) <$> show t) ++ post
-     _ <- f k
-     return k
-
-resolveSource ::
-  Map
-  -> IO FilePath
-resolveSource m =
-  foldSource return (\u -> withWorkFile "Source-" (extension' m) (\f -> wget ("-c " ++ u ++ " -O " ++ f))) $ source m
-
-decompress ::
-  Map
-  -> FilePath
-  -> IO FilePath
-decompress m p =
-  case compression m of
-    None -> return p
-    Bzip -> withWorkFile "Decompress-" (extension None (mtype m)) (\z -> bzip ("-dc " ++ p ++ " > " ++ z) >> return z)
-    _    -> error "todo" -- todo other compression
-
-cleanImg ::
-  IO ()
-cleanImg =
-  mapM_ rm ["63240000.img", "63240000.tdb", "areas.list", "template.args", "osmmap.img", "osmmap.tdb"]
-
-resolveMapType ::
-  MapType
-  -> FilePath
-  -> IO FilePath
-resolveMapType (OpenStreetMap i) f =
-  chdirw (do _ <- splitter (f ++ " --mapid=" ++ mid i)
-             _ <- mkgmap ("--add-pois-to-areas --reduce-point-density-polygon=8 --remove-short-arcs --route --transparent --gmapsupp -c template.args --description=\"Australia and Oceania and Mt Barney Contour\" --country-name=\"Australia and Oceania\" --country-abbr=AU --drive-on-left")
-             loop_
-               ((or <$>) . mapM doesFileExist)
-               (mapM_ rm)
-               ((\s -> [s ++ ".osm.gz", s ++ ".img"]) . mid <$> iterate incrementMapName i)
-             cleanImg
-             withWorkFile "Map-" ".img" (renameFile "gmapsupp.img"))
-resolveMapType (Pbf i) f =
-  chdirw (do _ <- splitter (f ++ " --mapid=" ++ mid i)
-             _ <- mkgmap ("--add-pois-to-areas --reduce-point-density-polygon=8 --remove-short-arcs --route --transparent --gmapsupp -c template.args --description=\"Australia and Oceania and Mt Barney Contour\" --country-name=\"Australia and Oceania\" --country-abbr=AU --drive-on-left")
-             loop_
-               ((or <$>) . mapM doesFileExist)
-               (mapM_ rm)
-               ((\s -> [s ++ ".osm.pbf.gz", s ++ ".img"]) . mid <$> iterate incrementMapName i)
-             cleanImg
-             withWorkFile "Map-" ".img" (renameFile "gmapsupp.img"))
-resolveMapType Garmin f =
-  return f
-
-resolveMap ::
-  Map
-  -> IO (FilePath, FilePath)
-resolveMap m =
-  do s <- resolveSource m 
-     d <- decompress m s
-     p <- resolveMapType (mtype m) d
-     unless (s == d) (rm d)
-     return (p, s)
-
-resolveMaps ::
-  [Map]
-  -> (FilePath -> ExitCode -> IO a)
-  -> IO a
-resolveMaps m f =
-  chdirw (do p <- mapM resolveMap m
-             e <- mkgmap (("--add-pois-to-areas --reduce-point-density-polygon=8 --remove-short-arcs --route --transparent --gmapsupp --description=\"Australia and Oceania and Mt Barney Contour\" --country-name=\"Australia and Oceania\" --country-abbr=AU --drive-on-left ") ++ intercalate " " (map fst p))
-             cleanImg
-             mapM_ (\(a, b) -> unless (a == b) (rm a)) p
-             z <- getCurrentDirectory
-             f (z </> "gmapsupp.img") e)
-
+  -> CreateProcess
+linkLatest d t =
+  procIn d "ln"
+    [
+      "-f"
+    , "-s"
+    , "-n"
+    , t
+    , "latest"
+    ]
--- src/Main.hs
+++ src/Main.hs
@@ -1,188 +1,31 @@
-module Main where
+module Main(
+  run
+, main
+) where
 
-import Control.Monad.Trans.Class
-import System.Environment
-import System.FilePath
-import Data.Time
-import Sys.Exit
-import Data.NotZero
-import Data.NotZeroOr
+import Control.Monad.Trans.Class(MonadTrans(lift))
+import Data.NotZero(notZeroElse1)
+import Data.NotZeroOr(NotZeroOr(IsNotZero))
+import Data.Time(getCurrentTime)
+import Geo.Garmin(time, commands, linkLatest)
+import System.Environment(getArgs)
+import Sys.Exit(ExitCodeM, createMakeWaitProcesses, createMakeWaitProcessM, exitWith, exit)
+import System.FilePath((</>))
+import System.IO(hPutStrLn, stderr)
 
-time ::
-  UTCTime
-  -> String
-time t =
-  let show2 = let s2 [x] = ['0', x]
-                  s2 x = x
-              in s2 . show
-      (y, m, d) = toGregorian (utctDay t)
-      TimeOfDay h n s = timeToTimeOfDay (utctDayTime t)
-  in concat [show y, show2 m, show2 d, "-", show2 h, show2 n, show2 (floor s)]
-
-getAustraliaOceania ::
-  FilePath
-  -> CreateProcess
-getAustraliaOceania wd =
-  procIn (wd </> "download") "wget"
-    [
-      "-q"
-    , "-c"
-    -- http://download.geofabrik.de/australia-oceania-latest.osm.pbf
-    , "http://localhost:8000/map.pbf"
-    , "-O"
-    , "australia-oceania.osm.pbf"
-    ]
-    
-splitAustraliaOceania ::
-  FilePath
-  -> CreateProcess
-splitAustraliaOceania wd =
-  procIn (wd </> "build" </> "australia-oceania") "java"
-    [
-      "-Xmx1536M"
-    , "-jar"
-    , "/home/tmorris/opt/splitter/splitter.jar"
-    , wd </> "download" </> "australia-oceania.osm.pbf"
-    , "--mapid=82345912"
-    ]
-
-gmapsuppAustraliaOceania ::
-  FilePath
-  -> CreateProcess
-gmapsuppAustraliaOceania wd =
-  procIn (wd </> "build" </> "australia-oceania") "java"
-    [
-      "-Xmx1536M"
-    , "-jar"
-    , "/home/tmorris/opt/mkgmap/mkgmap.jar"
-    , "--add-pois-to-areas"
-    , "--reduce-point-density-polygon=8"
-    , "--remove-short-arcs"
-    , "--route"
-    , "--transparent"
-    , "--gmapsupp"
-    , "-c"
-    , "template.args"
-    , "--description=\"Australia and Oceania\""
-    , "--country-name=\"Australia and Oceania\""
-    , "--region-name=\"Australia and Oceania\""
-    , "--region-abbr=AU"
-    , "--country-abbr=AU"
-    , "--drive-on=left"
-    , "--check-roundabouts"
-    ]
-
-linkAustraliaOceania ::
-  FilePath
-  -> CreateProcess
-linkAustraliaOceania wd =
-  procIn (wd </> "dist" </> "australia-oceania") "ln"
-    [
-      "-s"
-    , ".." </> ".." </> "build" </> "australia-oceania" </> "gmapsupp.img"
-    ]
-
-getMountBarney ::
-  FilePath
-  -> CreateProcess    
-getMountBarney wd =
-  procIn (wd </> "download") "wget"
-    [
-      "-q"
-    , "-c"
-    , "https://dl.dropboxusercontent.com/u/7810909/img/Mt_Barney_National_Park.img"
-    , "-O"
-    , "mt-barney-national-park.img"
-    ]
-
-gmapsuppMountBarneyAustraliaOceania ::
-  FilePath
-  -> CreateProcess
-gmapsuppMountBarneyAustraliaOceania wd =
-  procIn (wd </> "build" </> "australia-oceania_mt-barney") "java"
-    [
-      "-Xmx1536M"
-     , "-jar"
-     , "/home/tmorris/opt/mkgmap/mkgmap.jar"
-     , "--add-pois-to-areas"
-     , "--reduce-point-density-polygon=8"
-     , "--remove-short-arcs"
-     , "--route"
-     , "--transparent"
-     , "--gmapsupp"
-     , "--description=\"Australia and Oceania and Mt Barney Contour\""
-     , "--country-name=\"Australia and Oceania\""
-     , "--country-abbr=AU"
-     , "--region-name=\"Australia and Oceania\""
-     , "--region-abbr=AU"
-     , "--drive-on=left"
-     , "--check-roundabouts"
-     , wd </> "build" </> "australia-oceania" </> "gmapsupp.img"
-     , wd </> "download" </> "mt-barney-national-park.img"
-     ]
-
-linkAustraliaOceaniaMountBarney ::
-  FilePath
-  -> CreateProcess
-linkAustraliaOceaniaMountBarney wd =
-  procIn (wd </> "dist" </> "australia-oceania_mt-barney") "ln"
-    [
-      "-s"
-    , ".." </> ".." </> "build" </> "australia-oceania_mt-barney" </> "gmapsupp.img"
-    ]
-
-commands ::
-  FilePath
-  -> [CreateProcess]
-commands =
-  sequence
-    [
-      getAustraliaOceania
-    , splitAustraliaOceania
-    , gmapsuppAustraliaOceania
-    , linkAustraliaOceania
-    , getMountBarney
-    , gmapsuppMountBarneyAustraliaOceania
-    , linkAustraliaOceaniaMountBarney
-    ]
-  
-
-{-
-
-refactor non-IO
-mkgmap, splitter
-
--}
-
-
-linkLatest ::
-  FilePath
-  -> String
-  -> CreateProcess
-linkLatest d t =
-  procIn d "ln"
-    [
-      "-f"
-    , "-s"
-    , "-n"
-    , t
-    , "latest"
-    ]
-
-run2 ::
+run ::
   FilePath
   -> ExitCodeM IO
-run2 d =
+run d =
   do t <- lift getCurrentTime
      let u = time t
      createMakeWaitProcesses . commands . (</>) d $ u
      createMakeWaitProcessM (linkLatest d u)
 
-main2 ::
+main ::
   IO ()
-main2 =
+main =
   do a <- getArgs
      case a of
-       [] -> putStrLn "Usage: osmgarmin <output-dir>" >> exitWith (IsNotZero (notZeroElse1 127))
-       (o:_) -> exit (run2 o)
-       
+       [] -> hPutStrLn stderr "Usage: osmgarmin <output-dir>" >> exitWith (IsNotZero (notZeroElse1 127))
+       (o:_) -> exit (run o)
